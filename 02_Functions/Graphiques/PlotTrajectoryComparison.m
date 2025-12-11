ca40bb09b74462efdc17a9bbba7f72f432e568c0
Hugo Bocciarelli <hugobocciarelli@gmail.com>
2025-12-11 14:11:11 +0100

    Correction structure

--- 02_Functions/Graphiques/PlotTrajectoryComparison.m
+++ 02_Functions/Graphiques/PlotTrajectoryComparison.m
@@ -1,9 +1,9 @@
 
-function PlotTrajectoryComparison(TimeT, Corrected_states, Z, meas_time, meas_datetime)
+function PlotTrajectoryComparison(TimeT, Corrected_states, Position, Measures,meas_time, meas_datetime)
     time_meas_col = meas_datetime(:);
     
-    % 1. Calculer les mesures en ECI 
-    Meas_ECI = ECEFtoECI(Z, time_meas_col);
+    % 1. Calculer les positions dans ECI 
+    Position_ECI = ECEFtoECI(Position, time_meas_col);
     
     % 2. Extraire la position en sortie du filtre
     Calc_ECI = Corrected_states(1:3, :);
@@ -16,8 +16,10 @@ function PlotTrajectoryComparison(TimeT, Corrected_states, Z, meas_time, meas_da
     for k = 1:3
         subplot(3, 1, k);
         plot(TimeT, Calc_ECI(k, :), 'b-', 'LineWidth', 1.5);
+
         hold on;
-        plot(meas_time, Meas_ECI(k, :), 'ro', 'MarkerSize', 4, 'MarkerFaceColor', 'r');
+        plot(meas_time, Position_ECI(k, :), 'ro', 'MarkerSize', 2, 'MarkerFaceColor', 'r');
+        plot(meas_time, Measures(k, :), 'ro', 'MarkerSize', 2, 'MarkerFaceColor', 'g');
         grid on;
         ylabel(Composantes{k});
         if k == 1
--- 02_Functions/Reference_Frame/LatLonDist2ECEF.m
+++ 02_Functions/Reference_Frame/LatLonDist2ECEF.m
@@ -1,10 +1,10 @@
 function vec = LatLonDist2ECEF(Azimuth,elevation,Distance)
-%This function takes a list of azimut, elevation angle and a list of distance between obs and the object.
-%pos0 is a vector define as [Lat_obs,long_obs,h_obs]
-wgs84 = wgs84Ellipsoid('kilometers');
-lat0 = 45;
-lon0 = 0;
-h0 = 0;
-vec = aer2ecef(Azimuth,elevation,Distance,lat0,lon0,h0,wgs84);
-
+    %This function takes a list of azimut, elevation angle and a list of distance between obs and the object.
+    %pos0 is a vector define as [Lat_obs,long_obs,h_obs]
+    wgs84 = wgs84Ellipsoid('kilometers');
+    lat0 = 45;
+    lon0 = 0;
+    h0 = 0;
+    [x, y, z] = aer2ecef(Azimuth, elevation, Distance, lat0, lon0, h0, wgs84);
+    vec = [x(:)'; y(:)'; z(:)'];  
 end
